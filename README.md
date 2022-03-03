259e33ce47744016ab988a64c4995a811ba3b731
Jakob Ondrey <ondrey@gmail.com>
2022-03-02 21:33:55 -0600

    adds script to export api responses as .csv files

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # cps-extras
 Some one off scripts to pull CPS dtata
+
+CPS has a number of public APIs that are documented at [api.cps.edu](api.cps.edu).
+
+Contained in this repo are a number of scripts for calling them and processing the COVID health specific APIs. However, many of the functions here can be easily modified for use in other APIs.
+
+
+The scripts are meant to value readability over performance as to allow easier modification for whatever your personal use-case may be. 
+
+## Requirements and Dependencies ##
+
+- Python
+  - I am using `3.9` but im sure you will be okay with something earlier or later.
+- Pip
+
+## Virtual Environments ##
+
+Using a virtual environment is highly suggested. 
+
+In the root directory of this repo, create and activate a virtual environment with the following commands:
+
+```sh
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+And install additional requirements:
+```sh
+pip install -r requirements.txt
+```
+
+---
+
+
+## Contents of scripts ##
+
+### Full API Export - `full_export.py` ###
+This script will export the response of an api (or multiple APIs) as a .csv file. You can then open it in excel or another spreadsheet program if that is more to your liking.
+
+There is a list of API endpoints that are mostly commented out. The APIs that are NOT commented out will be called. Each API response will be saved with a file name formatted as `api_name`-`date-time`.csv.
---
+++ full_export.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+def save_as_csv(api_tag, response):
+    time = datetime.now() - timedelta(hours=6) #Central time offset
+    time = time.strftime("%Y-%m-%d-%H%M")
+    df = pd.read_json(response.text, orient="records")
+    filename = "{0}-{1}.csv".format(api_tag, time)
+    df.to_csv(filename, index=False)
+    print("{0} saved".format(filename))
+
+
+def export_data(apis, save_files):
+    print("Calling APIs. This can take some time")
+    for api_tag in apis.keys():
+        print("Calling: {0}".format(api_tag))
+        response = requests.get(apis[api_tag])
+        if save_files:
+            save_as_csv(api_tag, response)       
+
+apis = {
+    "district_weekly_actionable21": "https://api.cps.edu/health/cps/District2021WeeklyCovidActionable",
+    "district_daily_actionable21": "https://api.cps.edu/health/cps/District2021DailyCovidActionable",
+    "district_no_admit": "https://api.cps.edu/health/cps/DistrictNoAdmittanceSummary",
+    "School_no_admit": "https://api.cps.edu/health/cps/SchoolNoAdmittanceSummary",
+    "school_weekly_actionable": "https://api.cps.edu/health/cps/SchoolWeeklyCovidActionable",
+    "district_weekly_actionable": "https://api.cps.edu/health/cps/DistrictWeeklyCovidActionable",
+    "district_weekly_testing": "https://api.cps.edu/health/cps/District2021WeeklyCOVIDTesting",
+    "district_daily_testing": "https://api.cps.edu/health/cps/District2021DailyCOVIDTesting",
+    "school_daily_actionable": "https://api.cps.edu/health/cps/School2021DailyCovidActionable",
+    "school_daily_surveillance": "https://api.cps.edu/health/cps/SchoolDailyCOVIDSurveillance",
+    "network_daily_surveillance": "https://api.cps.edu/health/cps/NetworkDailyCOVIDSurveillance",
+    "district_daily_surveillance": "https://api.cps.edu/health/cps/DistrictDailyCOVIDSurveillance",
+}
+
+if __name__ == "__main__":
+    # save_files: save a time stampped copy of the api response as csv (True/False)
+    save_files = True
+
+    # returns a dictionary similar to apis above with the api name as the key and
+    # the raw response of the call as the value.
+    export_data(apis, save_files)
